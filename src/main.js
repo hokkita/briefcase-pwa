40b11b5ea3971c96fa039970acaa76f73886240f
Khairul Azry <mkhairulazry@gmail.com>
2023-01-10 10:33:51 +0800

    Setup PWA

--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import { createApp } from 'vue'
 import App from '@/App.vue'
 import '@/assets/css/tailwind.css'
 import router from '@/router/index.js';
+import { registerSW } from 'virtual:pwa-register'
+
+const updateSW = registerSW({
+    onNeedRefresh() {},
+    onOfflineReady() {},
+    immediate: true,
+  })
 
 const app = createApp(App);
 
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,47 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'url'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+
+    // PWA Configurations
+    VitePWA({ 
+      registerType: 'autoUpdate',
+      workbox: {
+        clientsClaim: true,
+        skipWaiting: true,
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+      },
+      devOptions: {
+        enabled: true
+      },
+      injectRegister: 'auto',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'BRIEFCASE',
+        shortname: 'BRIEFCASE',
+        description: 'Playground for Investor & Business Owner',
+        theme_color: '#222C3C',
+        icons: [
+          {
+            src: 'briefcase-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'briefcase-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+    // PWA Configurations Ends
+  ],
   // if got CNAME or deploying to https://<USERNAME>.github.io/
   base: '/',
   // if deploying to https://<USERNAME>.github.io/<REPO>/
